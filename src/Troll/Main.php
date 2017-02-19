efd095c9da65fd96d3366fd223920370c43a9907
BamYT <bamcraftpe3.0@gmail.com>
2017-02-19 21:22:41 +0100

    Update 1.0.2
    -Bug Fixes
    -neue Vanish Items
    - /v command

--- src/Troll/Main.php
+++ src/Troll/Main.php
@@ -2,6 +2,8 @@
 
 namespace Troll;
 
+use pocketmine\entity\Effect;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\command\Command;
@@ -19,13 +21,22 @@ class Main extends PluginBase implements Listener
     public function onEnable()
     {
         $this->getLogger()->info($this->prefix . "Wurde geladen");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onJoin(PlayerJoinEvent $event)
+    {
+        $player = $event->getPlayer();
+        $player->removeAllEffects();
+        $player->setGamemode(0);
+        $event->setJoinMessage("");
     }
 
     public function vitems(Player $player)
     {
         $player->getInventory()->clearAll();
-        $player->getInventory()->setItem(0, Item::get(341)->setCustomName(C::BOLD.C::GREEN."Vanish on"));
-        $player->getInventory()->setItem(8, Item::get(341)->setCustomName(C::BOLD.C::RED."Vanish off"));
+        $player->getInventory()->setItem(0, Item::get(341)->setCustomName(C::BOLD . C::GREEN . "Vanish on"));
+        $player->getInventory()->setItem(8, Item::get(358)->setCustomName(C::BOLD . C::RED . "Vanish off"));
     }
 
     public function onCommand(CommandSender $sender, Command $cmd, $label, array $args)
@@ -34,7 +45,7 @@ class Main extends PluginBase implements Listener
         switch ($cmd->getName()) {
             case "Vanish":
                 $this->vitems($sender);
-                $sender->sendMessage($this->prefix."Du hast die Vanish Items erhalten");
+                $sender->sendMessage($this->prefix . "Du hast die Vanish Items erhalten");
                 return true;
         }
     }
@@ -45,9 +56,23 @@ class Main extends PluginBase implements Listener
         $name = $player->getName();
         $item = $player->getInventory()->getItemInHand();
 
-        if($item->getName() == C::BOLD.C::GREEN."Vanish on");
+        if ($item->getName() == C::BOLD . C::GREEN . "Vanish on") ;
         {
-            $player->sendMessage($this->prefix."Du bist nun im Vanish");
+            $player->sendMessage($this->prefix . "Du bist nun im Vanish");
+            $effect = Effect::getEffect(Effect::INVISIBILITY);
+            $player->setGamemode(1);
+            $effect->setAmplifier(9999);
+            $effect->setDuration(9999);
+            $player->addEffect($effect);
+            $player->getInventory()->clearAll();
+        }
+
+        if($item->getName() == C::BOLD.C::RED."Vanish off")
+        {
+            $player->sendMessage($this->prefix."Du bist nun nicht mehr im Vanish");
+            $player->getInventory()->clearAll();
+            $player->setGamemode(0);
+            $player->removeAllEffects();
         }
     }
 
